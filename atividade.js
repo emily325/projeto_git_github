90a493c9fb1274509b2ae76d32dc963ccb713dcb
Emily <emilyrebeca1000@gmail.com>
2026-03-23 10:20:35 -0300

    web

---
+++ atividade.js
@@ -0,0 +1,14 @@
+var nome = 'Emily';
+console.log(nome)
+var sobrenome = 'Rebeca';
+console.log(sobrenome)
+
+var anoAtual = 2026;
+console.log(anoAtual)
+
+var idadeAteFinalDoAno = 19;
+console.log(idadeAteFinalDoAno); 
+
+console.log(`${nome} ${sobrenome}`);
+console.log(`${anoAtual - idadeAteFinalDoAno}`);
+
